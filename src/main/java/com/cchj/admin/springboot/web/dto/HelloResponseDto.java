a7530d78845f4c89ba86b1c59df3b3adfdcceb4c
maxbort <cjfgh2449@naver.com>
2023-01-13 17:30:49 +0900

    feat. 롬복 설치 및 롬복을 사용하기 위한 코드 수정 및 테스트 코드 작성

---
+++ src/main/java/com/cchj/admin/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.cchj.admin.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
